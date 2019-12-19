446e36a5bd1407330475bb875c6bcade3b2c3eb7
Alvaro Serrano <alvaroserrano@localhost.localdomain>
2019-12-19 18:51:40 -0500

    finished alert state and initialized github state

---
+++ src/components/layout/Alert.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import { directive } from "@babel/types";
+
+const Alert = () => {
+  const alertContext = useContext();
+  const { alert } = alertContext;
+  return (
+    alert !== null && (
+      <div className={`alert alert-${alert.type}`}>
+        <i className="fas fa-info-circle"></i> {alert.message}
+      </div>
+    )
+  );
+};
+
+export default Alert;
