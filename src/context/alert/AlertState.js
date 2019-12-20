1317ca294e4c2f99cf72463251beb64974e0461a
alvaroserrrano <alvaro.serrano@wmich.edu>
2019-12-20 04:56:27 -0500

    fixed alert state

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -5,21 +5,29 @@ import { SET_ALERT, REMOVE_ALERT } from "../types";
 
 const AlertState = props => {
   const initialState = null;
+
   const [state, dispatch] = useReducer(AlertReducer, initialState);
 
-  const setAlert = (message, type) => {
+  // Set Alert
+  const setAlert = (msg, type) => {
     dispatch({
       type: SET_ALERT,
-      payload: { message, type }
+      payload: { msg, type }
     });
-    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 4000);
 
-    return (
-      <AlertContext.Provider value={{ alert: state, setAlert }}>
-        {props.children}
-      </AlertContext.Provider>
-    );
+    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 5000);
   };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
 };
 
 export default AlertState;
